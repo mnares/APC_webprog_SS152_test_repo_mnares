430f22902cfd417c589a2736d8633a4db1cb4bbb
mnares <marcnares1118@gmail.com>
2016-11-06 01:48:05 +0800

    Removed some codes that arent useful

--- Exercise4/Form.php
+++ Exercise4/Form.php
@@ -24,38 +24,6 @@ body {
 
 }
 
-input[type=text], select {
-  width: 50%;
-  height: 10px;
-  padding: 12px 15px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-
-}
-
-textarea{
-  width: 230px;
-  height: 50px;
-  box-sizing: border-box;
-  border: 2px solid #ccc;
-  border-radius: 4px;
-  font-size: 16px;
-  background-color: white;
-  background-position: 10px 10px;
-  background-repeat: no-repeat;
-  padding: 12px 20px 12px 40px;
-  -webkit-transition: width 0.4s ease-in-out;
-  transition: width 0.4s ease-in-out;
-
-}
-
-textarea:focus {
-    width: 75%;
-}
-
 .input{
   position: absolute;
   left: 65%;
