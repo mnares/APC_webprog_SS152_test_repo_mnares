728d7643e71abc58b7159792b1ba9df6da5e98c6
mnares <marcnares1118@gmail.com>
2016-11-06 02:22:18 +0800

    Edited some codes

--- Exercise5/Homepage.php
+++ Exercise5/Homepage.php
@@ -115,8 +115,7 @@ function  info5(){
 
 	</div>
 
-	<div id = "trivia">
-		
+	<div id = "trivia">	
 		<h3 id ="triviaName">Trivia</h3>
 		
 		<form id="trivia1">
@@ -142,7 +141,7 @@ function  info5(){
 		<br>
 		<input class="button" type="button" value="Find out!" onclick="info3()"/>
 		<p id="answer3"></p>
-		<input class="button" type="button" value="Form Val." onclick="window.location='Form.php'"/>
+		
 		</form>
 
 		<form id="trivia4">
@@ -161,15 +160,12 @@ function  info5(){
 		<p id="answer5"></p>
 		</form>
 
-
+		<h3 id ="formName">Form Validation</h3>
+		<form id="form1"> 	
+		<input class="button" type="button" value="Form Validation" onclick="window.location='Form.php'"/>
+		</form>
 
 	</div>
-
-
-</div>
-
-
-	
 	
 </body>
 </html>
