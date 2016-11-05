3ff5a48f0d7be6a15a34242fda25852999210850
mnares <marcnares1118@gmail.com>
2016-11-06 01:46:27 +0800

    Removed some codes that arent useful

--- Exercise5/Form.php
+++ Exercise5/Form.php
@@ -36,26 +36,6 @@ input[type=text], select {
 
 }
 
-textarea{
-  width: 230px;
-  height: 50px;
-  box-sizing: border-box;
-  border: 2px solid #ccc;
-  border-radius: 4px;
-  font-size: 16px;
-  background-color: white;
-  background-position: 10px 10px;
-  background-repeat: no-repeat;
-  padding: 12px 20px 12px 40px;
-  -webkit-transition: width 0.4s ease-in-out;
-  transition: width 0.4s ease-in-out;
-
-}
-
-textarea:focus {
-    width: 75%;
-}
-
 .input{
   position: absolute;
   left: 65%;
