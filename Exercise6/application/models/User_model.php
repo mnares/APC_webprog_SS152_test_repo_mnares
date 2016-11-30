fc2f268b3f2d2532b8a428b052f143503107212e
mnares <marcnares1118@gmail.com>
2016-11-30 10:20:22 +0800

    Added User_models.php to models

---
+++ Exercise6/application/models/User_model.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class User_model extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    // create
+    function insert_user($data_users)
+    {
+        return $this->db->insert('user', $data_users);
+    }
+    
+}?>
