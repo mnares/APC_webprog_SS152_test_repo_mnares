3970e6e4a31ac24466384de24821d43d772a81bb
mnares <marcnares1118@gmail.com>
2016-12-16 09:39:10 +0800

    readded basic

--- Exercise7/basic/assets/AppAsset.php
+++ Exercise7/basic/assets/AppAsset.php
@@ -15,22 +15,12 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-   // public $basePath = '@webroot';
-   // public $baseUrl = '@web';
-    public $sourcePath = '@bower/grayscale/';
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
     public $css = [
-        'vendor/bootstrap/css/bootstrap.min.css',
-            'vendor/font-awesome/css/font-awesome.min.css',
-                'https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic',
-                    'https://fonts.googleapis.com/css?family=Montserrat:400,700',
-                        'css/grayscale.min.css'
+        'css/site.css',
     ];
     public $js = [
-        'vendor/jquery/jquery.js',
-        'vendor/bootstrap/js/bootstrap.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js',
-        'js/grayscale.min.js',
-        '',
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- Exercise7/basic/config/web.php
+++ Exercise7/basic/config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'DoingItEasy',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
